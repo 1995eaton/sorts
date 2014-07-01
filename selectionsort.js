0c7ab5dcc922adfe000ec4c4839a3b42dabc62f3
1995eaton <1995eaton@gmail.com>
2014-07-01 18:46:21 -0400

    redundant

--- selectionsort.js
+++ selectionsort.js
@@ -8,7 +8,7 @@
 selectionSort = function(array) {
   var a = array.slice(0);
   var i, j, len, min;
-  for (j = 0, len = a.length, min; j < len - 1; j++) {
+  for (j = 0, len = a.length; j < len - 1; j++) {
     for (i = j + 1, min = j; i < len; i++) {
       if (a[i] < a[min]) {
         min = i;
